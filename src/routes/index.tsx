e87ee723ea58d6ae1acf7f5fbda1595e63a3eff1
Rahul Palamarthi <87606332+rahulpeacock@users.noreply.github.com>
2025-06-02 22:17:10 +0530

    update: tailwindcss vite plugin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <div className='p-2'>
-      <h3>Welcome Home!</h3>
+      <h3 className='text-2xl font-semibold'>Welcome Home!</h3>
       <Button asChild>
         <Link to='/about'>About</Link>
       </Button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
