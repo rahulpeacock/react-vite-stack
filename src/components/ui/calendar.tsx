3d81cfbfcde180ee8cf6df26e77e8121fd17f943
Rahul Palamarthi <87606332+rahulpeacock@users.noreply.github.com>
2025-06-03 19:43:14 +0530

    fix: icons in calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,7 +3,7 @@ import { DayPicker } from 'react-day-picker';
 
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 
 function Calendar({ className, classNames, showOutsideDays = true, ...props }: React.ComponentProps<typeof DayPicker>) {
   return (
