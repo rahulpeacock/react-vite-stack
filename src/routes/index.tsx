fb0c0720eb41d6a0d4733597ba097201714ebf10
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2025-08-10 10:53:59 +0530

    refactor: remove unnecessary routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
-import { BarSpinner, SCLoader } from '@/components/global/loaders';
+import { SCLoader } from '@/components/global/loaders';
 import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/')({
@@ -11,17 +11,9 @@ function Index() {
     <div className='p-2'>
       <h3 className='text-2xl font-semibold'>Welcome Home!</h3>
       <Button asChild>
-        <Link to='/about'>
-          <BarSpinner /> About
-        </Link>
-      </Button>
-      <Button asChild>
         <Link to='/posts'>
           <SCLoader /> Posts
         </Link>
-      </Button>
-      <Button asChild>
-        <Link to='/posts/post'>Individual Post</Link>
       </Button>
     </div>
   );
